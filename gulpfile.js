2654fa4ab4d803d2b2930ad5290dd28dc3ac91c9
Dayan ramly Ramadhan <dayzramly@gmail.com>
2016-12-01 10:26:17 +0700

    fix image compressing

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ uglify = require('gulp-uglify'),
 rename = require('gulp-rename'),
 concat = require('gulp-concat'),
 imagemin = require('gulp-imagemin'),
+pngquant = require('imagemin-pngquant'),
 include = require('gulp-include'),
 jshint = require('gulp-jshint'),
 cssnano = require('gulp-cssnano'),
@@ -34,6 +35,13 @@ gulp.task('styles', function(){
     .pipe(gulp.dest(OutputCss));
     // .pipe(notify({ message: 'Styles task complete' }));
 });
+//styles non minify
+gulp.task('stylesNoMinify', function(){
+    return sass(StylesCss, {style:'expanded', noCache:true})
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest(OutputCss));
+    // .pipe(notify({ message: 'Styles task complete' }));
+});
 // scripts
 gulp.task('scripts', function(){
     return gulp.src(JsFiles)
@@ -53,6 +61,11 @@ gulp.task('fonts', function(){
 // image
 gulp.task('images', function(){
     return gulp.src(ImageFiles)
+    .pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+    }))
     .pipe(gulp.dest(OutputImg));
 })
 
@@ -61,10 +74,11 @@ gulp.task('clean', function(){
 });
 
 gulp.task('default', ['clean'], function(){
-    gulp.start('styles', 'scripts', 'fonts', 'images');
+    gulp.start('styles', 'stylesNoMinify', 'scripts', 'fonts', 'images');
 });
 
 gulp.task('watch', function(){
     gulp.watch(CssFiles,['styles']);
     gulp.watch(AllJsFiles,['scripts']);
+    gulp.watch(ImageFiles,['images']);
 })
